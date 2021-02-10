e35bf362e5f7f5462551e11fb355e1875bb8e197
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-02-10 08:20:14 +0200

    Добавляе 6 домашку

---
+++ script/task-6.js
@@ -0,0 +1,16 @@
+const validationInputRef = document.querySelector('#validation-input');
+
+validationInputRef.addEventListener('blur', onInputCheck);
+
+function onInputCheck(event) {
+  const lengthInput = event.currentTarget.value.length;
+  const dataAttributeInput = Number(
+    validationInputRef.getAttribute('data-length'),
+  );
+
+  if (lengthInput < dataAttributeInput) {
+    validationInputRef.classList = 'invalid';
+  } else {
+    validationInputRef.classList = 'valid';
+  }
+}
